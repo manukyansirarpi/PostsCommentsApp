eb9f268d8ef2a58649cdcd18548f5cb8a85c5f26
Manukyan Sirarpi <manukyansirarpi@gmail.com>
2022-11-10 17:19:46 +0400

    fix formatting

--- src/components/posts/postsAPI.ts
+++ src/components/posts/postsAPI.ts
@@ -2,4 +2,4 @@ import { PostI } from "./postsSlice";
 
 export function fetchPosts() : Promise<PostI[]> { 
       return fetch('https://jsonplaceholder.typicode.com/posts').then((res) => res.json()); 
-}   
+}
--- src/components/users/usersAPI.ts
+++ src/components/users/usersAPI.ts
@@ -2,5 +2,5 @@ import { UserI } from "./usersSlice";
 
 export function fetchUsers(): Promise<UserI[]> {
   return fetch('https://jsonplaceholder.typicode.com/users').then((res) => res.json());
-  }
+}
   
